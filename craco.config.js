ec9965183717c28fffdb15502f0c4ed9975b0732
Vansh Maggo <lucifersays11@gmail.com>
2026-09-10 15:20:22 +0530

    Fix craco config to avoid requiring terser-webpack-plugin, remove overrides

--- craco.config.js
+++ craco.config.js
@@ -1,11 +1,9 @@
-const TerserPlugin = require("terser-webpack-plugin");
-
 module.exports = {
   webpack: {
     configure: (webpackConfig) => {
       // Disable TerserPlugin (minification) to avoid ajv/schema-utils conflicts
       webpackConfig.optimization.minimizer = webpackConfig.optimization.minimizer.filter(
-        (plugin) => !(plugin instanceof TerserPlugin)
+        (plugin) => plugin.constructor.name !== 'TerserPlugin'
       );
       return webpackConfig;
     },
